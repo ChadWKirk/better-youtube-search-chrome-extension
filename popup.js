90025d710dc6c5fde4ca6d8c7afdbb76a7495fd9
Chad Kirk <ChadWKirk@gmail.com>
2023-05-05 12:29:04 -0500

    Created on/off slider functionality. On/Off toggle slider now works.

--- popup.js
+++ popup.js
@@ -1,18 +1,42 @@
-function validate() {
-  if (document.getElementById("checkbox").checked) {
-    console.log("check");
-    chrome.storage.local.set({ [isOn]: true }).then((result) => {
-      console.log(chrome.storage);
+//checkbox in popup.html
+//checkbox is 0 opacity so you can't see it, but it is represented by the toggle slider
+let checkbox = document.getElementById("checkbox");
+
+//checks chrome.storage.local for isOn to enable or disable extension in new session
+//so extension maintains whether it was on or off the last time the user used Chrome
+//so they don't have to turn it on again each time they open Chrome
+function checkIfTurnedOn() {
+  chrome.storage.local.get(["isOn"]).then((result) => {
+    if (result.isOn) {
+      checkbox.checked = true;
+      console.log(result);
+    } else {
+      checkbox.checked = false;
+      console.log(result);
+    }
+  });
+}
+
+checkIfTurnedOn();
+
+//turns extension on and off with toggle slider in popup html
+//so user can easily enable or disable the extension without having to go to their extensions tab to do so
+function toggleOffOn() {
+  if (checkbox.checked) {
+    chrome.storage.local.set({ isOn: true }).then((result) => {
+      chrome.storage.local.get(["isOn"]).then((result) => {
+        console.log(result);
+      });
+      console.log("is checked");
     });
   } else {
     chrome.storage.local.set({ isOn: false }).then((result) => {
-      console.log(result, " result");
+      chrome.storage.local.get(["isOn"]).then((result) => {
+        console.log(result);
+      });
+      console.log("is not checked");
     });
   }
 }
 
-// if (chrome.storage.local.get(["isOn"])) {
-//   document.getElementById("checkbox").checked = true;
-// }
-
-document.querySelector("#checkbox").addEventListener("click", validate);
+checkbox.addEventListener("click", toggleOffOn);
--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   chrome.storage.local.get(["isOn"]).then((result) => {
-    if (
-      result &&
-      tab.url &&
-      tab.url.includes("youtube.com/results?search_query=") &&
-      !tab.url.includes("&sp=")
-    ) {
-      chrome.tabs.update({ url: tab.url + "&sp=CAASAhAB" });
-      console.log(result, " result");
+    if (result.isOn) {
+      if (
+        tab.url &&
+        tab.url.includes("youtube.com/results?search_query=") &&
+        !tab.url.includes("&sp=")
+      ) {
+        chrome.tabs.update({ url: tab.url + "&sp=CAASAhAB" });
+      }
+    } else {
+      return;
     }
   });
 });
