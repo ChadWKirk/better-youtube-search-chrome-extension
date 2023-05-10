0ee1c289ab150c4a1cddef5e8a510bf9481209b0
Chad Kirk <ChadWKirk@gmail.com>
2023-05-10 14:46:52 -0500

    Added message in content.js to trigger event to add url flag when searching for the same thing as when user searched for when extension was turned off.

--- scripts/background.js
+++ scripts/background.js
@@ -13,3 +13,23 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     }
   });
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // chrome.storage.local.get(["isOn"]).then((result) => {
+  // if (result.isOn) {
+  //   if (
+  //     tab.url &&
+  //     tab.url.includes("youtube.com/results?search_query=") &&
+  //     !tab.url.includes("&sp=")
+  //   ) {
+  if (request.message === "submit") {
+    chrome.tabs.update({ url: tab.url + "&sp=CAASAhAB" });
+    sendResponse();
+  }
+
+  //   }
+  // } else {
+  //   return;
+  // }
+  // });
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,24 @@
-// if (window.location.href.includes("search_query=")) {
-//   console.log(window.location.href);
-// }
+//send a message to background.js whenever the search-form submits a search
+//this triggers the extension to add the flag to the end of the URL
+//so it adds the flag when user searches for the same thing twice, when extension was off for the first search but is turned on for the 2nd search
+
+let searchForm = document.getElementById("search-form");
+let searchBtn = document.getElementById("search-icon-legacy");
+
+function enterSubmitFunction() {
+  if (e.key === "Enter") {
+    chrome.runtime.sendMessage({ message: "submit" }, function (response) {
+      console.log(response);
+    });
+  }
+}
+
+function clickSubmitFunction() {
+  chrome.runtime.sendMessage({ message: "submit" }, function (response) {
+    console.log(response);
+  });
+}
+
+searchForm.addEventListener("keypress", enterSubmitFunction);
+
+searchBtn.addEventListener("click", clickSubmitFunction);
