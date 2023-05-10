0b91f5441dcbd6b7817b9f4ea0c494af7a9382e6
Chad Kirk <ChadWKirk@gmail.com>
2023-05-10 11:25:51 -0500

    Made it so slider doesn't slide into position upon opening popup. Did this by controlling transition time with javascript.

--- popup.js
+++ popup.js
@@ -11,23 +11,28 @@ let onLabel = document.getElementsByClassName("on-label");
 //to set their transition times to 0 when initially bringing up the extension
 //to make it instantly be in either the off or on position instead of sliding up or down when opening extension popup
 let slider = document.getElementsByClassName("slider");
-let sliderBallTransition = window
-  .getComputedStyle(document.querySelector(".slider"), ":before")
-  .getPropertyValue("transition");
+let sliderBallTransition = document.querySelector("::before");
+
+let universal = document.querySelectorAll("*");
 
 //checks chrome.storage.local for isOn to enable or disable extension in new session
 //so extension maintains whether it was on or off the last time the user used Chrome
 //so they don't have to turn it on again each time they open Chrome
 //also changes icon in toolbar and topbar stuff from red to grey color scheme when extension is turned off and vice versa
+//also changes slider::before's transition time
 function checkIfTurnedOn() {
   chrome.storage.local.get(["isOn"]).then((result) => {
     if (result.isOn) {
       checkbox.checked = true;
+      console.log(sliderBallTransition);
       offLabel[0].style.color = "#a3a3a3";
       onLabel[0].style.color = "#000";
       chrome.action.setIcon({
         path: "./images/better-youtube-search-icon-16.png",
       });
+      setTimeout(() => {
+        document.documentElement.style.setProperty("--transition", "0.2s");
+      }, 300);
       document.getElementById("topbar").style.backgroundColor = "#eb0000";
       document.getElementById("star").style.opacity = "1";
       document.getElementById("triangle").style.opacity = "1";
@@ -43,6 +48,7 @@ function checkIfTurnedOn() {
       chrome.action.setIcon({
         path: "./images/better-youtube-search-icon-16-grey.png",
       });
+      document.documentElement.style.setProperty("--transition", "0.2s");
       document.getElementById("topbar").style.backgroundColor = "#e7e7e7";
       document.getElementById("star").style.opacity = "0";
       document.getElementById("triangle").style.opacity = "0";
@@ -61,6 +67,7 @@ checkIfTurnedOn();
 //turns extension on and off with toggle slider in popup html
 //so user can easily enable or disable the extension without having to go to their extensions tab to do so
 //also changes icon in toolbar and topbar stuff from red to grey color scheme when extension is turned off and vice versa
+//also changes slider::before's transition time
 function toggleOffOn() {
   if (checkbox.checked) {
     chrome.storage.local.set({ isOn: true }).then((result) => {
@@ -72,6 +79,17 @@ function toggleOffOn() {
       chrome.action.setIcon({
         path: "./images/better-youtube-search-icon-16.png",
       });
+      document.getElementById("topbar").style.transition = "0.2s";
+      document.querySelectorAll("span")[0].style.transition = "0.2s";
+      document.querySelectorAll("span")[1].style.transition = "0.2s";
+      document.querySelectorAll("span")[2].style.transition = "0.2s";
+      document.getElementById("star").style.transition = "0.2s";
+      document.getElementById("triangle").style.transition = "0.2s";
+      document.getElementById("mag").style.transition = "0.2s";
+      document.querySelector(".on-label").style.transition = "0.2s";
+      document.querySelector(".off-label").style.transition = "0.2s";
+      document.querySelector(".slider").style.transition = "0.2s";
+      document.documentElement.style.setProperty("--transition", "0.2s");
       document.getElementById("topbar").style.backgroundColor = "#eb0000";
       document.querySelectorAll("span")[0].style.color = "#282828";
       document.querySelectorAll("span")[1].style.color = "#fff";
@@ -91,6 +109,17 @@ function toggleOffOn() {
       chrome.action.setIcon({
         path: "./images/better-youtube-search-icon-16-grey.png",
       });
+      document.getElementById("topbar").style.transition = "0.2s";
+      document.querySelectorAll("span")[0].style.transition = "0.2s";
+      document.querySelectorAll("span")[1].style.transition = "0.2s";
+      document.querySelectorAll("span")[2].style.transition = "0.2s";
+      document.getElementById("star").style.transition = "0.2s";
+      document.getElementById("triangle").style.transition = "0.2s";
+      document.getElementById("mag").style.transition = "0.2s";
+      document.querySelector(".on-label").style.transition = "0.2s";
+      document.querySelector(".off-label").style.transition = "0.2s";
+      document.querySelector(".slider").style.transition = "0.2s";
+      document.documentElement.style.setProperty("--transition", "0.2s");
       document.getElementById("topbar").style.backgroundColor = "#e7e7e7";
       document.getElementById("star").style.opacity = "0";
       document.getElementById("triangle").style.opacity = "0";
