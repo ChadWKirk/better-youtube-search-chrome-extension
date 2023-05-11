965755f2c5ea2bcb08792b6dfa95359690bfba90
Chad Kirk <ChadWKirk@gmail.com>
2023-05-11 15:12:53 -0500

    Added message sender for when youtube search button is clicked, so when a user searches for something with ext off then turns it on and searches for the same thing it still adds the flag even though the URL isn't being updated since the search terms are the same

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,4 @@
+//for when tab is updated
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   chrome.storage.local.get(["isOn"]).then((result) => {
     if (result.isOn) {
@@ -14,22 +15,31 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
   });
 });
 
+//for when youtube search button is clicked
+//this makes it so that if a user searches for something with the extension off, and then turns it on and searches for the exact same thing, it still adds the flag
+//even though the url isn't being updated since the search terms are the same
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // chrome.storage.local.get(["isOn"]).then((result) => {
-  // if (result.isOn) {
-  //   if (
-  //     tab.url &&
-  //     tab.url.includes("youtube.com/results?search_query=") &&
-  //     !tab.url.includes("&sp=")
-  //   ) {
-  if (request.message === "submit") {
-    chrome.tabs.update({ url: tab.url + "&sp=CAASAhAB" });
-    sendResponse();
-  }
-
-  //   }
-  // } else {
-  //   return;
-  // }
-  // });
+  chrome.storage.local.get(["isOn"]).then((result) => {
+    if (result.isOn) {
+      if (request.message === "submit") {
+        //timeout is just to make it change URL at a specific time to make it look more smooth for the user
+        setTimeout(() => {
+          chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tab) {
+              if (
+                tab[0].url &&
+                tab[0].url.includes("youtube.com/results?search_query=") &&
+                !tab[0].url.includes("&sp=")
+              ) {
+                chrome.tabs.update({ url: tab[0].url + "&sp=CAASAhAB" });
+              }
+            }
+          );
+        }, 200);
+      }
+    } else {
+      console.log("proceed");
+    }
+  });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,11 @@
-//send a message to background.js whenever the search-form submits a search
+//send a message to background.js whenever the search button is clicked
 //this triggers the extension to add the flag to the end of the URL
 //so it adds the flag when user searches for the same thing twice, when extension was off for the first search but is turned on for the 2nd search
 
-let searchForm = document.getElementById("search-form");
 let searchBtn = document.getElementById("search-icon-legacy");
 
-function enterSubmitFunction() {
-  if (e.key === "Enter") {
-    chrome.runtime.sendMessage({ message: "submit" }, function (response) {
-      console.log(response);
-    });
-  }
-}
-
-function clickSubmitFunction() {
+searchBtn.addEventListener("click", () => {
   chrome.runtime.sendMessage({ message: "submit" }, function (response) {
-    console.log(response);
+    console.log("message sent");
   });
-}
-
-searchForm.addEventListener("keypress", enterSubmitFunction);
-
-searchBtn.addEventListener("click", clickSubmitFunction);
+});
